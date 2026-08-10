b5092f125af4a9bbd93fb2b115364e718e42c7e7
Hagno1310 <hoangnh.ptit@gmail.com>
2026-08-10 22:53:34 +0700

    feat: update environment configuration and Vite setup for improved port management

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,22 @@
-import { defineConfig } from 'vite'
+import { defineConfig, loadEnv } from 'vite'
 import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
-export default defineConfig({
-  plugins: [react()],
+export default defineConfig(({ mode }) => {
+  // load tat ca bien trong .env (ke ca bien khong co prefix VITE_) + process.env
+  const env = loadEnv(mode, process.cwd(), '')
+  const devPort = Number(env.DEV_PORT) || 5173
+  const webPort = Number(env.WEB_PORT) || 6666
+
+  return {
+    plugins: [react()],
+    server: {
+      host: true,
+      port: devPort,
+    },
+    preview: {
+      host: true,
+      port: webPort,
+    },
+  }
 })
